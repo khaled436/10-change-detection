41203ef140eb45d966a647d712de291e326d14d8
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-05-28 16:18:22 +0200

    203 async pipe

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject, DestroyRef} from '@angular/core';
 import {MessagesService} from "../messages.service";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-messages-list',
@@ -7,23 +8,16 @@ import {MessagesService} from "../messages.service";
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    AsyncPipe
+  ]
 })
-export class MessagesListComponent implements  OnInit {
+export class MessagesListComponent{
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
 
-  messages: string[] = [];
+  messages$ = this.messagesService.messages$;
 
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    })
-    this.destroyRef.onDestroy(()=>{
-      subscription.unsubscribe();
-    })
-  }
+
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
